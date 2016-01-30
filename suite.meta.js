029bbbe1d6ce2b967406cd8e93a8474618a6905d
theoddball <oddball@oddball.tf>
2016-01-29 18:14:50 -0700

    Update suite.meta.js

--- suite.meta.js
+++ suite.meta.js
@@ -1,15 +1,15 @@
 /*!
 // ==UserScript==
 // @name         backpack.tf enhancement suite
-// @namespace    http://steamcommunity.com/id/caresx/
-// @author       cares
-// @version      1.5.0.1
+// @namespace    http://steamcommunity.com/id/caresx/ http://steamcommunity.com/id/theoddball
+// @author       cares with edits by The Oddball
+// @version      1.5.1a
 // @description  Enhances your backpack.tf experience.
 // @include      /^https?://.*\.?backpack\.tf/.*$/
 // @exclude      /^https?://forums\.backpack\.tf/.*$/
 // @require      https://caresx.github.io/backpacktf-enhancement-suite/deps.js
-// @downloadURL  https://caresx.github.io/backpacktf-enhancement-suite/suite.user.js
-// @updateURL    https://caresx.github.io/backpacktf-enhancement-suite/suite.meta.js
+// @downloadURL  http://oddball.tf/img/oddsBES.user.js
+// @updateURL    http://oddball.tf/img/oddsBES.meta.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
