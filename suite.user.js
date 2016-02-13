19d880a6c2681e0b9bede97f299985539fbed05d
theoddball <oddball@oddball.tf>
2016-02-13 06:34:51 -0700

    .1
    fixed

--- suite.user.js
+++ suite.user.js
@@ -3,7 +3,7 @@
 // @name         backpack.tf enhancement suite
 // @namespace    http://steamcommunity.com/id/caresx/ http://steamcommunity.com/id/theoddball
 // @author       cares with edits by The Oddball
-// @version      1.6.2a
+// @version      1.6.2.1
 // @description  Enhances your backpack.tf experience.
 // @include      /^https?://.*\.?backpack\.tf/.*$/
 // @exclude      /^https?://forums\.backpack\.tf/.*$/
