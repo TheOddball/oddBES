11130b053e69e83a9c3520fb117acdca299682e2
The Oddball <oddball@oddball.tf>
2016-03-13 11:30:55 -0700

    Revert "v1.6.7.1"
    This reverts commit ce96e545b0dbb1f0aaeb4d72451aa744c3fdbf32.

--- suite.meta.js
+++ suite.meta.js
@@ -1,15 +1,15 @@
 /*!
 // ==UserScript==
-// @name         Backpack.tf Enhancement Suite
+// @name         backpack.tf enhancement suite
 // @namespace    http://steamcommunity.com/id/theoddball
-// @author       The Oddball
-// @version      1.6.7.1
+// @author       cares with edits by The Oddball
+// @version      1.6.7
 // @description  Enhances your backpack.tf experience.
 // @include      /^https?://.*\.?backpack\.tf/.*$/
 // @exclude      /^https?://forums\.backpack\.tf/.*$/
 // @require      https://caresx.github.io/backpacktf-enhancement-suite/deps.js
-// @downloadURL  https://github.com/theoddball/oddBES/raw/gh-pages/suite.user.js
-// @updateURL    https://github.com/theoddball/oddBES/raw/gh-pages/suite.meta.js
+// @downloadURL  https://theoddball.github.io/oddBES/suite.user.js
+// @updateURL    https://theoddball.github.io/oddBES/suite.meta.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
